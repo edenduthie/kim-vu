7aa289b69525a0d598c5bade49d764ec093acd88
Eden Duthie <eduthie@gmail.com>
2013-08-28 11:10:12 +1000

    Switched to meteor

--- custom-bootstrap/compile.sh
+++ custom-bootstrap/compile.sh
@@ -1,3 +1,3 @@
 lessc bootstrap.less > bootstrap.css
-cp bootstrap.css ../kim-vu/css/bootstrap.css
-cp ../bootstrap/dist/js/bootstrap.js ../kim-vu/js/bootstrap.js
+cp bootstrap.css ../kim-vu/client/lib/bootstrap.css
+cp ../bootstrap/dist/js/bootstrap.js ../kim-vu/client/lib/bootstrap.js
