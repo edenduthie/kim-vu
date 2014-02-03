6913335b0b303e737a4b1a8145af186f8214d321
Eden Duthie <eduthie@gmail.com>
2014-02-03 05:27:13 +0000

    Updated run

--- kim-vu/run.sh
+++ kim-vu/run.sh
@@ -2,4 +2,4 @@
 cd /root/code/kim-vu/kim-vu
 export PATH=$PATH:/usr/local/bin
 export HOME=/root
-nohup /usr/local/bin/meteor --port 8023 >/dev/null 2>&1 &
+nohup /usr/local/bin/meteor --port 80 >/dev/null 2>&1 &
